d5049a4438db8043ec44dc42d6511b3fe1912438
Aaron Barthel <aaron.barthel@live.ca>
2020-09-17 14:03:38 -0600

    Aarons Component

---
+++ src/components/AaronComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => {
+
+  return (
+    <p>Aarons Component, Hi Aaron :)</p>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import AaronsComponent from './components/AaronComponent';
+
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-   
+    <AaronsComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
