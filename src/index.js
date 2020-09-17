911ab9d7ba6379235c5cd2ee7fd959305feddae6
Erpreet <preetkaurchd15@gmail.com>
2020-09-17 14:03:00 -0600

    Added HelloPreet component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloPreet from './components/HelloPreet';
 
 
 ReactDOM.render(
   <React.StrictMode>
-   
+   <HelloPreet />
   </React.StrictMode>,
   document.getElementById('root')
 );
